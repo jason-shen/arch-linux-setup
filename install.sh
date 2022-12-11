cd9128ab1bd8e4a05298604425eab4f9e5d5c6ff
Jason Shen <jason_2000_nz@hotmail.com>
2022-12-11 21:04:47 +1300

    first commit

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
 # install package
 # pacman -S <package name>
 # uninstall package
@@ -14,6 +18,9 @@
 
 clear
 
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
 echo "PostInstall starts now"
 # install video drivers
 # amd gpu
@@ -24,17 +31,66 @@ echo "PostInstall starts now"
 # pacman -Syu --noconfirm nvidia nvidia-tools
 # vm gpu
 pacman -Syu --noconfirm xf86-video-qxl
-pacman -S --noconfirm xorg xorg-xinit git alacritty bspwm sxhkd nitrogen picom firefox dmenu lxsession arandr
+pacman -S --noconfirm --needed base-devel
+pacman -S --noconfirm xorg-server xorg-xinit git && \
+  alacritty picom firefox dmenu lxsession arandr && \
+  fad x11-xserver-utils unzip wget pulseaudio && \
+  pavucontrol flameshot neovim lxappearance sddm 
+
+
+echo "installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install yay -- AUR package mananger
+echo "installing yay"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -rf yay
+
+echo "installing leftwm"
+yay --noconfirm -S leftwm
 
 echo "creating config folders"
-mkdir ~/.config/bspwm
-mkdir ~/.config/sxhkd
+mkdir ~/.config/leftwm
+mkdir ~/.config/
 
 echo "copying default config files"
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-cp /usr/share/doc/sxhkdrc/examples/bspwmrc ~/.config/sxhkd/
+cp /usr/share/doc/sxhkd/examples/background_shell/sxhkdrc ~/.config/sxhkd/
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+echo "installing fonts"
+# Installing fonts
+cd "$builddir" || exit
+nala install fonts-font-awesome -y
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d "/home/$username/.fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d "/home/$username/.fonts"
+mv dotfonts/fontawesome/otfs/*.otf "/home/$username/.fonts/"
+chown "$username:$username" "/home/$username/.fonts/*"
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+echo "installing cursor"
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors || exit
+./install.sh
+cd "$builddir" || exit
+rm -rf Nordzy-cursors
+
+# Enable graphical login and change target from CLI to GUI
+tar -xzvf slice.tar.gz --strip 1 --one-top-level=/usr/share/sddm/themes/slice
+cp -f "$builddir/sddm.conf" /etc/
+systemctl enable sddm
+systemctl set-default graphical.target
+
 
 
 
